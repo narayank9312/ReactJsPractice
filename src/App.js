f78d57bb18f6b148ee03993b2dbc16d1ec224a3f
Narayan Kumar Jha <narayank9312@gmail.com>
2021-03-02 15:48:05 +0530

    useState learned
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  //let count = 1;
+  // const state = useState();
+  //console.log(state);
+
+  const [count, setCount] = useState(0);
+
+  const IncNUM = () => {
+    setCount(count + 1);
+    //console. log('clicked ' + count++);
+  };
+  // const purple = '#8e44ad';
+  // const [bg, setBg] = useState(purple);
+
+  // const bgChange = () => {
+  //   //console.log('clicked');//
+  //   //const purple = '#8e44ad';
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{background: 'bg'}}>
+        <h1> {count} </h1>
+        <button onClick={IncNUM}> Click Me </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
